88523dd9434c29cb53db5834dc69926a672d0e06
at-atanasov <at.atanasov@gmail.com>
2024-02-04 13:15:09 +0200

    handling GET requests

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'HEllo from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'HEllo from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.status(200).send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    results: tours.length,
+    data: {
+      tours, // тъй като името на пропъртито съвпада с това на променливата не е необходимо да я посочваме
+    },
+  });
 });
 
 const port = 3000;
