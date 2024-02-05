c4ae15f51e034c80c1e3244fa0a17915f362a8a4
at-atanasov <at.atanasov@gmail.com>
2024-02-05 20:07:23 +0200

    responding to URL parameters

--- app.js
+++ app.js
@@ -29,6 +29,28 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  //добавяме параметърът по този начин, после го достъпваме с req.params като обект напр. { id: '9' }
+  console.log(req.params);
+
+  const id = req.params.id * 1; // това е добър трик за превръщането на стринг в число
+
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'succes',
+    data: {
+      tour, // тъй като името на пропъртито съвпада с това на променливата не е необходимо да я посочваме
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   //   console.log(req.body);
 
