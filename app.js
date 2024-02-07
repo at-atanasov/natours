611684d08b937a122770568196442a0ebcd549d1
at-atanasov <at.atanasov@gmail.com>
2024-02-07 23:31:48 +0200

    better file structure

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+const app = express();
 
 //1 Middlewares
 app.use(morgan('dev'));
@@ -25,125 +23,6 @@ app.use((req, res, next) => {
 
 //2 Route handlers
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'succes',
-    results: tours.length,
-    requestTime: req.requestTime,
-    data: {
-      tours, // тъй като името на пропъртито съвпада с това на променливата не е необходимо да я посочваме
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  //добавяме параметърът по този начин, после го достъпваме с req.params като обект напр. { id: '9' }
-  console.log(req.params);
-
-  const id = req.params.id * 1; // това е добър трик за превръщането на стринг в число
-
-  const tour = tours.find((el) => el.id === id);
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'succes',
-    data: {
-      tour, // тъй като името на пропъртито съвпада с това на променливата не е необходимо да я посочваме
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  //   console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body); //creating a new object by merging two existing objects
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<updated tour here ...>',
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null, // we send null in order to show that the object no longer exist
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
 //3 Routes
 
 // app.get('/api/v1/tours', getAllTours);
@@ -152,21 +31,8 @@ const deleteUser = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-tourRouter.route('/').get(getAllTours).post(createTour);
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 //4 Start server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App runing on port ${port}...`);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    results: tours.length,
+    requestTime: req.requestTime,
+    data: {
+      tours, // тъй като името на пропъртито съвпада с това на променливата не е необходимо да я посочваме
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  //добавяме параметърът по този начин, после го достъпваме с req.params като обект напр. { id: '9' }
+  console.log(req.params);
+
+  const id = req.params.id * 1; // това е добър трик за превръщането на стринг в число
+
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'succes',
+    data: {
+      tour, // тъй като името на пропъртито съвпада с това на променливата не е необходимо да я посочваме
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  //   console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body); //creating a new object by merging two existing objects
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<updated tour here ...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null, // we send null in order to show that the object no longer exist
+  });
+};
