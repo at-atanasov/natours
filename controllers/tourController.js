42999e7ea6f907ca2cfe8afb0685e136632fcad0
at-atanasov <at.atanasov@gmail.com>
2024-02-24 15:39:11 +0200

    making the API better - aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,12 @@ const Tour = require('./../models/tourModel');
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 // );
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
