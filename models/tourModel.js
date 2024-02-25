91efa019292fbc4ca34272cc795130a8aaf57f27
at-atanasov <at.atanasov@gmail.com>
2024-02-25 21:28:49 +0200

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration!'],
@@ -64,6 +66,17 @@ tourSchema.virtual('durationWeeks').get(function () {
   //виртуалните пропъртита не се пазят в базата данни. В случая се създава при гет метод след пресмятане върху поле duration.
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema); //Creating new model based on tourSchema. Model names begin with capital letter(convention).
 
