40dfc10ec2eafc893cbdb17c8571c6a6c64a9257
at-atanasov <at.atanasov@gmail.com>
2024-03-07 22:44:58 +0200

    better errors and refactoring

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -44,22 +46,14 @@ app.all('*', (req, res, next) => {
   //   message: `Can't find ${req.originalUrl} on this server!`,
   // });
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  next(err); //if next receuves argument express knows that this is an error
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)); //if next receuves argument express knows that this is an error
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 //4 Start server
 module.exports = app;
