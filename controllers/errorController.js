cb31840aa87be7f6ccda5bb9e9e9f94a450c71a8
at-atanasov <at.atanasov@gmail.com>
2024-03-09 23:24:14 +0200

    handling duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,11 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   console.log(err);
   res.status(err.statusCode).json({
@@ -44,6 +49,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err, name: err.name };
     console.log(error);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     sendErrorProd(error, res);
   }
 };
