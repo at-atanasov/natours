adfb98ebbcb3b9503e672f841c6e92da8d7cc2f7
at-atanasov <at.atanasov@gmail.com>
2024-03-10 09:12:02 +0200

    handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,6 +10,13 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   console.log(err);
   res.status(err.statusCode).json({
@@ -50,6 +57,8 @@ module.exports = (err, req, res, next) => {
     console.log(error);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
