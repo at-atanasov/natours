6b1d82c72bf557a66f1cc84a428f9a3cdff0acf0
at-atanasov <at.atanasov@gmail.com>
2024-03-10 10:03:54 +0200

    Catching uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION ☠️ Shuting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -25,8 +31,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION ☠️ Shuting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
